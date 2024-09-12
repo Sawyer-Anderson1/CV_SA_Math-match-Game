494d18caf75b4f926c6cb67663618557859eae60
Sawyer-Anderson1 <sawyerlynnanderson@icloud.com>
2024-09-12 13:59:55 -0500

    Update board.asm

--- src/board.asm
+++ src/board.asm
@@ -5,4 +5,40 @@
 # Description: The file that controls the board
 #-----------------------------------------------------------
 
+#-----------------------
+# Declare some constants
+#-----------------------
+.data
+columnHeader: .asciiz "_|0 1 2 3"
+rowHeader: .asciiz "   	 |"
+		"	0|"
+		"	1|"
+		"	2|"
+		"	3|"
+string3: .asciiz "\nLoop #"
 
+#------------------
+# Main program body
+#------------------
+.text
+main:
+li $v0, 4
+la $a0, string1
+syscall
+la $a0, string2
+syscall
+li $t0, 1
+loop:
+li $v0, 4
+la $a0, string3
+syscall
+li $v0, 1
+move $a0, $t0
+syscall
+addi $t0, $t0, 1
+bne $t0, 4, loop
+#-----
+# Halt
+#-----
+li $v0, 10
+syscall
