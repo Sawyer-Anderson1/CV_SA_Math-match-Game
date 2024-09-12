f8e4514c3e6cac6df3554b0f3f3109211edf6de0
Sawyer-Anderson1 <sawyerlynnanderson@icloud.com>
2024-09-12 14:39:47 -0500

    Update board.asm

--- src/board.asm
+++ src/board.asm
@@ -8,19 +8,31 @@
 #-----------------------
 # Declare some constants
 #-----------------------
-.data
-columnHeader: 	.asciiz "_|0 1 2 3"
-rowHeader: 	.asciiz "0|+ + + +"
-			"1|+ + + +"
-			"2|+ + + +"
-			"3|+ + + +"
-.include 
+		.data
+columnHeader: 	.asciiz "_|0 1 2 3\n"
+row0: 		.asciiz "0|+ + + +\n"
+row1:		.asciiz "1|+ + + +\n"
+row2:		.asciiz "2|+ + + +\n"
+row3:		.asciiz "3|+ + + +\n"
+
 #------------------
 # Main program body
 #------------------
 .text
-bne $t0, #, boardUpdate
-
-boardUpdate:
-bne $t0, #, boardUpdate
-
+main: #dummy main
+li	$v0, 4
+la	$a0, columnHeader
+syscall
+la	$a0, row0
+syscall
+la	$a0, row1
+syscall
+la	$a0, row2
+syscall
+la	$a0, row3
+syscall
+#li	$v0, 4
+#la	$s0, row3
+#syscall
+#boardUpdate:
+#bne $t0, 5, boardUpdate #will change the amount it iterates when we get to working on the other files in the furtue
