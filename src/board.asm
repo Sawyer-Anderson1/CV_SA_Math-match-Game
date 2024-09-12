373d4b0a5c0f056f77cd8584d07ffca89eefa0ad
Sawyer-Anderson1 <sawyerlynnanderson@icloud.com>
2024-09-12 14:22:05 -0500

    Update board.asm

--- src/board.asm
+++ src/board.asm
@@ -14,29 +14,13 @@ rowHeader: 	.asciiz "0|+ + + +"
 			"1|+ + + +"
 			"2|+ + + +"
 			"3|+ + + +"
-
+.include 
 #------------------
 # Main program body
 #------------------
 .text
-main:
-li $v0, 4
-la $a0, string1
-syscall
-la $a0, string2
-syscall
-li $t0, 1
-loop:
-li $v0, 4
-la $a0, string3
-syscall
-li $v0, 1
-move $a0, $t0
-syscall
-addi $t0, $t0, 1
-bne $t0, 4, loop
-#-----
-# Halt
-#-----
-li $v0, 10
-syscall
+bne $t0, #, boardUpdate
+
+boardUpdate:
+bne $t0, #, boardUpdate
+
