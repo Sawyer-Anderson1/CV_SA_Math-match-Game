89aafd5b5fc4671f51c740579fa75a17145f5f98
Sawyer-Anderson1 <sawyerlynnanderson@icloud.com>
2024-09-12 13:55:53 -0500

    Update board.asm

--- src/board.asm
+++ src/board.asm
@@ -1,1 +1,8 @@
+#-----------------------------------------------------------
+# Program File: board.asm
+# Written by: Sawyer Anderson
+# Date Created: 9/12/2024
+# Description: The file that controls the board
+#-----------------------------------------------------------
+
 
