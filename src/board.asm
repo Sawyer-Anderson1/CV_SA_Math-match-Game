e75d69d5952c33d0b5a6b10bac5379c5a2d2434d
Sawyer-Anderson1 <sawyerlynnanderson@icloud.com>
2024-09-26 14:27:36 -0500

    Update board.asm

--- src/board.asm
+++ src/board.asm
@@ -9,27 +9,98 @@
 # Declare some constants
 #-----------------------
 		.data
+prompt		.asciiz "Enter a row index 0-3 and a column index 0-3: "
 columnHeader: 	.asciiz "_|0 1 2 3\n"
 row0: 		.asciiz "0|+ + + +\n"
 row1:		.asciiz "1|+ + + +\n"
 row2:		.asciiz "2|+ + + +\n"
 row3:		.asciiz "3|+ + + +\n"
 
+i_index:	.word 0
+j_index: 	.word 0
+count:		.word 0
+amount:		.word 8
+
+displayCards:	.asciiz "3", "1x1", "1x2", "2x2", "1", "2", "1x3", "1x5", "4", "6", "5", "1x7", "4x2", "8", "7", "3x2" 
+cards:		.word 3, 1, 2, 4, 1,  2, 3, 5, 4, 6, 5, 7, 8, 8, 7, 6
+		     #0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15
+		 #row:0  0  0  0  1  1  1  1  2  2  2  2  3  3  3  3
+	     #column:0  1  2  3  0  1  2  3  0  1  2  3  0  1  2  3
+
 #------------------
 # Main program body
 #------------------
+
 .text
-main: #dummy main
-li	$v0, 4
-la	$a0, columnHeader
-syscall
-la	$a0, row0
-syscall
-la	$a0, row1
-syscall
-la	$a0, row2
-syscall
-la	$a0, row3
-syscall
-#boardUpdate:
-#bne $t0, 5, boardUpdate #will change the amount it iterates when we get to working on the other files in the furtue
+main:
+	lw	$s0, count
+	lw	$s1, amount
+	lw 	$s2, cards
+	
+	j	_boardUpdate
+	
+cardCheck:
+	
+	
+	#$t0 for the boolean
+	
+	#if card1 == card2:
+	#	boolean = 1
+	#else:
+	#	boolean = 0
+	
+	beq	$t0, $zero, _boardUpdate
+	
+	# incrementing the count
+	addi	$s0, $s0, 1
+	
+	j	_boardUpdate
+
+_boardUpdate:
+	
+	beq	$s0, $s1, exit 
+
+	li	$v0, 4
+	la	$a0, columnHeader
+	syscall
+	la	$a0, row0
+	syscall
+	la	$a0, row1
+	syscall
+	la	$a0, row2
+	syscall
+	la	$a0, row3
+	syscall
+	
+	la	$a0, prompt
+	syscall
+	
+	li	$v0, 1
+	syscall
+	move	$t0, $v0
+	lw	$t0, i_index
+	
+	li	$v0, 1
+	syscall
+	move	$t0, $v0
+	lw	$t0, j_index
+	
+	la	$a0, prompt
+	syscall
+	
+	li	$v0, 1
+	syscall
+	move	$t0, $v0
+	lw	$t0, i_index
+	
+	li	$v0, 1
+	syscall
+	move	$t0, $v0
+	lw	$t0, j_index
+	
+	j	cardCheck
+	
+exit: 
+	li	$v0, 10
+	syscall
+
