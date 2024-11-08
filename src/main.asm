b8396b528d46b5155aadcc256b07246bb7d78a25
Sawyer-Anderson1 <sawyerlynnanderson@icloud.com>
2024-11-07 18:05:19 -0600

    Update main.asm

--- src/main.asm
+++ src/main.asm
@@ -1,8 +1,9 @@
 #-----------------------------------------------------------
-# Program File: board.asm
+# Program File: main.asm
 # Written by: Sawyer Anderson and Carlos Vazquez
-# Date Created: 9/12/2024
-# Description: The file that controls the board
+# Date Created: 11/7/2024
+# Description: The file that gets input (Prompt) and calls
+#        the locationCheck and boardUpdate files/subroutines
 #-----------------------------------------------------------
 
 #-----------------------
@@ -10,58 +11,102 @@
 #-----------------------
 		.data
 
-matchIndicator: .asciiz "Match!\n"
-columnHeader: 	.asciiz "_|0 1 2 3\n"
-row0: 		.asciiz "0|+ + + +\n"
-row1:		.asciiz "1|+ + + +\n"
-row2:		.asciiz "2|+ + + +\n"
-row3:		.asciiz "3|+ + + +\n"
+prompt:		.asciiz "Enter a row index 0-3 and a column index 0-3: "
 
-displayCardsR0:	.asciiz "3", "1x1", "1x2", "2x2"
-displayCardsR1:	.asciiz "1", "2", "1x3", "1x5"
-displayCardsR2:.asciiz "4", "6", "5", "1x7"
-displayCardsR3:.asciiz "4x2", "8", "7", "3x2" 
+firstCard:	.word 0, 0
+secondCard: 	.word 0, 0
+amount:		.word 8
+timer:		.word 0
 
 #------------------
 # Main program body
 #------------------
 
 .text
-.globl TempPrint, MatchPrint, currBoard
-
-# Registers for TempPrint/MatchPrint:
-#	a0: the firstCard (the row index)
-# 	a1: the column index for the first card
-# 	a2: the secondCard (the row index)
-#	a3: the column index for the second card
-
-TempPrint: # showing the cards choosen, if they don't match
+.globl	main, Prompt
+main:
+	lw	$s0, amount
 	
-	j	Prompt
+Prompt: # reprompting for the input
+	jal	currBoard
 	
-MatchPrint: # permanantly change the board
-		
 	li	$v0, 4
-	la	$a0, matchIndicator
+	la	$a0, prompt
 	syscall
-		
-	j	Prompt
 	
+	li	$v0, 5
+	syscall
+	move	$t0, $v0
+	sw	$t0, firstCard
 	
-# Registers for boardUpdate:
-# 	
-
-currBoard:
+	li	$v0, 5
+	syscall
+	move	$t0, $v0
+	sw	$t0, firstCard+4
+	
 	li	$v0, 4
-	la	$a0, columnHeader
-	syscall
-	la	$a0, row0
-	syscall
-	la	$a0, row1
-	syscall
-	la	$a0, row2
-	syscall
-	la	$a0, row3
+	la	$a0, prompt
 	syscall
 	
-	jr	$ra
+	li	$v0, 5
+	syscall
+	move	$t0, $v0
+	sw	$t0, secondCard
+		
+	li	$v0, 5
+	syscall
+	move	$t0, $v0
+	sw	$t0, secondCard+4
+		
+cardCheck:	
+	# t0: the firstCard (the row index)
+	# t1: the column index for the first card
+	# t2: the secondCard (the row index)
+	# t3: the column index for the second card
+	# v0: the value of card 1 & 2
+	
+	lw	$t0, firstCard
+	lw	$t1, firstCard+4 #the column index for the first card
+	add	$a0, $zero, $t0 # the arugment for the subroutine call
+	add	$a1, $zero, $t1 # the second 
+	li	$a2, 0 # the argument that indicates that the operation is loading for first card
+	
+	jal	locationCheck # jumping to the subroutine
+	move	$s2, $v0
+	
+	lw	$t2, secondCard
+	lw	$t3, secondCard+4 #the column index for the second card
+	add	$a0, $zero, $t2
+	add	$a1, $zero, $t3
+	li	$a2, 0 # the value that indicates the second card
+	
+	jal	locationCheck # jumping to the subroutine
+	move	$s1, $v0
+
+	#the arguements for both MatchPrint and TempPrint	
+		#card 1
+	move	$a0, $t0
+	move	$a1, $t1
+		#card 2
+	move 	$a2, $t2
+	move	$a3, $t3
+		
+	#the if statement condition
+	bne	$s2, $s1, Else
+	
+	If: 
+		# decrementing the count
+		addi	$s0, $s0, -1
+		
+		#the exit condition
+		beq	$s0, -1, Exit # when all the cards have been matched a flipped
+		
+		j 	MatchPrint
+	Else:
+		li	$a0, 0 # 0 for not matching
+		
+		j	TempPrint
+	
+Exit: 
+	li	$v0, 10
+	syscall
